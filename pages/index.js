11d7693504e6786be2d44f0149e34efdb5223c86
MantuanoBruno <153833263+MantuanoBruno@users.noreply.github.com>
2025-12-19 01:04:08 +0000

    Adds `eslint` and config with `prettier` && `.eslintignore`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
   return <h1>Victoria, eu amo você. se me ama tbm imita uma gaivota 😝A</h1>;
 }
-
+function gaivota() {
+  return <h1>Gaivota imitada!</h1>;
+}
 export default Home;
+export { gaivota };
